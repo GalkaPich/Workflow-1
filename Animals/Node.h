9d48bbf1efcdfdb530fca9e7e3c43934df1ca05a
Max Belov <mpui@tut.by>
2014-12-22 15:42:46 +0300

    Update Node.h

--- Animals/Node.h
+++ Animals/Node.h
@@ -1,31 +1,74 @@
 #pragma once
 #include <string>
-class Node
-{
+#include <iostream>
+#include <cstdio>
+#include <cstddef>
+
+using namespace std;
+
+bool yesOrNo(std::string question) {
+    std::string res;
+    cout << question << endl;
+    getline(cin,res);
+    while ( (res !="y") && (res!="n") ) {
+        cout << "Please, type 'y' or 'n'\n" << question << endl;
+        getline(cin,res);
+    }
+    return res=="y";
+}
+
+
+class Node {
 public:
-	Node() {};
-	virtual ~Node() {};
-	virtual void ask() = 0;
+    virtual ~Node() {};
+    virtual bool ask(Node** parent=nullptr)=0;
 };
+
 class Question : public Node {
 public:
-	Question(std::string text_, Node* y, Node* n)
-		: text(text_), yes(y), no(n) {};
-	virtual ~Question() {
-		delete yes;
-		delete no;
-	}
-	virtual void ask();
-	const std::string text;
-	Node* yes;
-	Node* no;
+    const string text;
+    Node* yes;
+    Node* no;
+
+    Question(string text_, Node* y, Node* n)
+        : text(text_), yes(y), no(n) {};
+    virtual ~Question() {
+        delete yes;
+        delete no;
+    }
+
+    virtual bool ask(Node** parent) {
+        if (yesOrNo(text)) {
+            return yes->ask(&yes);
+        } else {
+            return no->ask(&no);
+        }
+    }
 };
 
 class Answer : public Node {
 public:
-	Answer(std::string text_) : text(text_) {};
-	virtual ~Answer() {};
-	virtual void ask();
-	const std::string text;
-};
+    const string text;
 
+    Answer(string text_) : text(text_) {};
+    virtual ~Answer() {};
+
+    virtual bool ask(Node** parent) {
+        if (yesOrNo("It is "+text+"?") ) {
+            cout << "Hooray! I've guessed\n";
+        } else {
+            string answer;
+            string question;
+            cout<<"Who is it?"<<endl;
+            getline(cin,answer);
+            cout<<"what is true for "<<answer<<" and is false for "<<text << endl;
+            getline(cin,question);
+            *parent=new Question(
+                question,
+                new Answer(answer),
+                this
+            );
+        }
+        return yesOrNo("Play again?\n");
+    }
+};
